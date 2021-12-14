074819653a1ad390ff3bd4609e8d91210a75230d
azimut <azimut.github@protonmail.com>
2021-12-13 22:02:07 -0300

    day12: replaced gethash to with href, on iterate

--- src/day12silver.lisp
+++ src/day12silver.lisp
@@ -7,20 +7,20 @@
                 "start"
                 "end")))
 
+(defun dead-end-p (edge counter)
+  (and (change-case:string-lower-case-p edge)
+       (= 1 (href-default 0 counter edge))))
+
 ;; I don't know what I am doing...sorta
 ;; https://www.youtube.com/watch?v=TlYExiAAbHo&t=742s
 (defun find-paths (adjacencies counter from to)
   (if (string/= from to)
-      (iter (for adj in (gethash from adjacencies))
+      (iter (for adj in (href adjacencies from))
         (when (not (dead-end-p adj counter))
-          (setf (gethash from counter) 1)
+          (setf (href counter from) 1)
           (summing (find-paths adjacencies counter adj to))
-          (setf (gethash from counter) 0)))
+          (setf (href counter from) 0)))
       1))
-
-(defun dead-end-p (edge counter)
-  (and (change-case:string-lower-case-p edge)
-       (= 1 (href-default 0 counter edge))))
 
 (defun day12-silver-parse (file)
   (->> file slurp lines (mapcar (curry #'split "-"))))
